1441a4682dd6f2c8fba710cbfa16902d902d0f8d
alexinh0 <alexferna07@gmail.com>
2021-12-23 11:42:07 +0100

    ball-paddle collision

--- Code/main_oud.asm
+++ Code/main_oud.asm
@@ -14,12 +14,15 @@ MODEL FLAT, C
 ASSUME cs:_TEXT,ds:FLAT,es:FLAT,fs:FLAT,gs:FLAT
 
 INCLUDE "global.asm"
-; INCLUDE "KEYB.asm"		; library custom keyboard handler
+INCLUDE "keyb.inc"		; library custom keyboard handler
 
 ; -------------------------------------------------------------------
 ; CODE
 ; -------------------------------------------------------------------
 CODESEG
+
+; # TODO LIJST #
+; - ALS DE BAL DE ONDERKANT VAN DE SCHERMRAAKT BEGINT DEZE WEER OP ZIJN STARTPOSITIE SAMEN MET DE PADDLE (DE BALL IS DAN NIET MEER ACTIEF EN VERLIEST 1 LEVEN ALS DEZE ER NOG HEEFT ANDERS IS HET SPEL GEDAAN)
 
 ; video mode aanpassen
 PROC setVideoMode
@@ -46,10 +49,10 @@ PROC waitForSpecificKeystroke
 	ARG 	@@key:byte
 	USES 	eax
 
-	@@waitForKeystroke:
-		mov	ah,00h
-		int	16h
-		cmp	al,[@@key]
+@@waitForKeystroke:
+	mov	ah,00h
+	int	16h
+	cmp	al,[@@key]
 	jne	@@waitForKeystroke
 
 	ret
@@ -80,7 +83,7 @@ PROC openFile ; de offset van een variabele neemt 32 bits in beslag
 	mov ah, 3dh ; mode om een bestand te openen
 	int 21h
 	
-	jnc @@no_error ; carry flag is set if error occurs, indien de CF dus niet geactieveerd is, is er geen error en springt men naar de no_error label
+	jnc SHORT @@no_error ; carry flag is set if error occurs, indien de CF dus niet geactieveerd is, is er geen error en springt men naar de no_error label
 
 	; Print string.
 	call setVideoMode, 03h ; plaatst mode weer in text mode 
@@ -104,7 +107,7 @@ PROC closeFile
 	mov ah, 3Eh ; mode om een bestand te sluiten
 	int 21h
 	
-	jnc @@no_error ; carry flag is set if error occurs
+	jnc SHORT @@no_error ; carry flag is set if error occurs
 
 	call setVideoMode, 03h
 	mov  ah, 09h
@@ -128,7 +131,7 @@ PROC readChunk
 	mov ah, 3fh								
 	int 21h
 	
-	jnc @@no_error  	
+	jnc SHORT @@no_error  	
 	
 	call setVideoMode, 03h
 	mov  ah, 09h
@@ -167,48 +170,232 @@ PROC fillBackground
 ENDP fillBackground
 
 STRUC Ball
-	x			db 0 ; (in cellen)
-	y			db 0
-	breadth		db BALLWIDTH/CELLWIDTH ; (in cellen)
-	height		db BALLHEIGHT/CELLHEIGHT
+	x			db BALLSTARTX ; min_x: 0, max_x: 78 (in cellen)
+	y			db BALLSTARTY ; min_y: 0, max_y: 48
+	active		db 0 ; 0 bal beweegt nog niet alleen (beweegt dus samen met paddle), 1 bal beweegt wel alleen
+	x_sense		db LEFT
+	y_sense		db UP
 ENDS Ball
 
 STRUC Paddle
-	x 			db 0
-	y 			db 0
-	breadth 	db PADDLEWIDTH/CELLWIDTH ; aangezien width een keyword is, gebruiken we breadth
-	height 		db PADDLEHEIGHT/CELLHEIGHT
+	x 			db PADDLESTARTX
+	y 			db PADDLESTARTY
 	health 		db 3 
 ENDS Paddle
 
 STRUC Stone
-	index_position 	db 0		; index in grid
-	breadth			db STONEWIDTH/CELLWIDTH
-	height			db STONEHEIGHT/CELLHEIGHT
-ENDS Stone	 
+	index 			db 0	; index in array
+	alive			db 1
+ENDS Stone
 
-; PROC gamelogistic
+PROC movePaddleLeft
+	USES eax, ebx
+	mov ebx, offset paddle_object
+	movzx eax, [ebx + Paddle.x]
+	cmp eax, 0
+	je SHORT @@end
+	dec eax
+	mov [ebx + Paddle.x], al
+	mov ebx, offset ball_object			; checken of de bal mee moet bewegen
+	movzx eax, [ebx + Ball.active]
+	cmp eax, 1
+	je SHORT @@end
+	call moveBallLeft
+@@end:
+	ret
+ENDP movePaddleLeft	
 
-	; mov al, [offset __keyb_keyboardState + 4Dh]		; state van rechterpijl bijhouden
-	; cmp al, 1
-	; je @@moveRight
-		
-	; mov al, [offset __keyb_keyboardState + 4Bh]		; state van linkerpijl bijhouden
-	; cmp al, 1
-	; je @@moveLeft
-		
-	; @@moveRight:
-		; ; call movePaddleRight
-		
-	; @@moveLeft:
-		; ; call movePaddleLeft
- 
-; ENDP gamelogistic 
+PROC movePaddleRight
+	USES eax, ebx
+	mov ebx, offset paddle_object
+	movzx eax, [ebx + Paddle.x]
+	cmp eax, BOARDWIDTH-PADDLEWIDTHCELL ; x-waarde van paddle-object vergelijken met grootst mogelijke x-waarde voor het paddle-object 
+	je SHORT @@end
+	inc eax
+	mov [ebx + Paddle.x], al
+	; checken of de bal mee moet bewegen
+	mov ebx, offset ball_object
+	movzx eax, [ebx + Ball.active]
+	cmp eax, 1
+	je SHORT @@end
+	call moveBallRight
+@@end:
+	ret
+ENDP movePaddleRight
 
-; ; Generische tekenprocedure die struct verwacht
+PROC moveBallLeft ; simpele versie van move, gebruikt door paddle in het begin van de spel
+	USES eax, ebx
+	mov ebx, offset ball_object
+	movzx eax, [ebx + Ball.x]
+	dec eax
+	mov [ebx + Ball.x], al
+	ret
+ENDP moveBallLeft
+
+PROC moveBallRight ; simpele versie van move, gebruikt door paddle in het begin van de spel
+	USES eax, ebx
+	mov ebx, offset ball_object
+	movzx eax, [ebx + Ball.x]
+	inc eax
+	mov [ebx + Ball.x], al
+	ret
+ENDP moveBallRight
+
+PROC moveBall
+
+; TODO:
+
+;; STAP1: ball houdt enkel rekening met schermgrenzen
+
+; ; VOOR LATER: NIET VERGETEN OM BIJ ELKE BEWEGINGSRICHTING TE CHECKEN OF DE BALL EEN STONE RAAKT!!!
+
+; ; ; DOWN:
+; ; checken of de bal zich onder de denkbeeldige lijn bevindt (zie oranje lijn tekening)
+		; ; => zo ja, check of deze zich juist boven de paddle bevindt
+				; ; => zo ja, check of er een match is tussen het bereik van da ball en de paddle volgens de x-as
+						; ; => zo ja, wijzig de beweginsrichting volgens de y-as, de ball beweeegt nu terug naar boven
+						; ; => zo nee, beweeg de ball volgens zijn huidige richting 
+				; ; => zo nee (dan bevindt deze zich onder of naast de paddle), check of deze de onderkant van de scherm raakt 
+						; ; => zo ja, decrement het aantal levens van de bal en check of het aantal levens = 0
+								; ; => zo ja, het spel is gedaan (zorg ervoor dat men dit weet a.d.h.v. een return-waarde van moveBall zodat men weet dat de game-loop gedaan is)
+								; ; => zo nee, plaats de paddle en de ball weer op hun startpositie
+
+; ; ; UP:
+; ; checken of de bal de bovenkant raakt => zo ja, wijzig de beweginsrichting volgens de y-as, de ball beweeegt nu naar beneden
+; ; ; LEFT:
+; ; checken of de bal de linkerkant raakt => zo ja, wijzig de beweginsrichting volgens de x-as, de ball beweeegt nu naar rechts
+; ; ; RIGHT:
+; ; checken of de bal de rechterkant raakt => zo ja, wijzig de beweginsrichting volgens de x-as, de ball beweeegt nu naar links
+
+	USES eax, ebx, ecx, edx
+	mov ebx, offset ball_object
+	movzx eax, [ebx + Ball.x_sense]
+	cmp eax, LEFT
+	je @@handleMoveLeft
+	jmp @@handleMoveRight
+	
+@@handleMoveLeft:
+	movzx eax, [ebx + Ball.x]
+	cmp eax, 0
+	jne @@moveLeft
+	mov [ebx + Ball.x_sense], RIGHT
+	jmp @@handleMoveVertical
+@@moveLeft:
+	dec eax
+	mov [ebx + Ball.x], al
+	jmp @@handleMoveVertical
+
+@@handleMoveRight:
+	movzx eax, [ebx + Ball.x]
+	cmp eax, BOARDWIDTH-BALLWIDTHCELL
+	jne @@moveRight
+	mov [ebx + Ball.x_sense], LEFT
+	jmp @@handleMoveVertical
+@@moveRight:
+	inc eax
+	mov [ebx + Ball.x], al
+	jmp @@handleMoveVertical
+
+@@handleMoveVertical:
+	movzx eax, [ebx + Ball.y_sense]
+	cmp eax, UP
+	je @@handleMoveUp
+	jmp @@handleMoveDown
+	
+@@handleMoveUp:
+	movzx eax, [ebx + Ball.y]
+	cmp eax, 0
+	jne @@moveUp
+	mov [ebx + Ball.y_sense], DOWN
+	jmp @@end
+@@moveUp:
+	dec eax
+	mov [ebx + Ball.y], al
+	jmp @@end
+	
+@@handleMoveDown:
+	movzx eax, [ebx + Ball.y]
+	cmp eax, PADDLESTARTY-BALLHEIGHTCELL
+	jg @@belowPaddle
+	jl @@moveDown
+	movzx ecx, [ebx + Ball.x]
+	mov edx, offset paddle_object
+	movzx edx, [edx + Paddle.x]
+	sub edx, BALLWIDTHCELL 						; x-coördinaat van de ball met BALLWIDTHCELL verhogen is equivalent met de x-coördinaat van de paddle met BALLWIDTHCELL te verminderen
+	cmp ecx, edx
+	jl @@moveDown								; er is geen botsing, de ball zit links van de paddle
+	mov edx, offset paddle_object
+	movzx edx, [edx + Paddle.x]
+	add edx, PADDLEWIDTHCELL
+	cmp ecx, edx
+	jle @@downToUp								; er is een botsing, de ball zit NIET rechts van de paddle 
+@@moveDown:
+	inc eax
+	mov [ebx + Ball.y], al
+	jmp @@end
+@@belowPaddle:
+	cmp eax, BOARDHEIGHT-BALLHEIGHTCELL
+	jne @@moveDown
+@@downToUp:
+	mov [ebx + Ball.y_sense], UP
+	
+	
+; @@handleMoveDown:
+	; movzx eax, [ebx + Ball.y]
+	; cmp eax, BOARDHEIGHT-BALLHEIGHTCELL
+	; jne @@moveDown
+	; mov [ebx + Ball.y_sense], UP
+	; jmp @@end
+; @@moveDown:
+	; inc eax
+	; mov [ebx + Ball.y], al
+
+@@end:
+	ret
+ENDP moveBall
+
+;; SPELLOGICA
+PROC gamelogistic
+	USES eax, ebx
+	
+	mov ebx, offset ball_object
+	mov al, [ebx + Ball.active]
+	cmp al, 0
+	je SHORT @@handle_input 
+	call moveBall									; bal beweegt enkel alleen als deze actief is
+
+@@handle_input:
+	mov al, [offset __keyb_keyboardState + 39h]		; state van spatiebalk bijhouden 
+	cmp al, 1										; (kan misschien beter, aangezien deze later ook nog kan getriggerd worden)
+	je SHORT @@makeBallActive
+
+	mov al, [offset __keyb_keyboardState + 4Dh]		; state van rechterpijl bijhouden
+	cmp al, 1
+	je SHORT @@moveRight
+		
+	mov al, [offset __keyb_keyboardState + 4Bh]		; state van linkerpijl bijhouden
+	cmp al, 1
+	je SHORT @@moveLeft
+	
+	jmp SHORT @@end
+
+@@makeBallActive:
+	mov [ebx + Ball.active], 1		; op actief zetten
+	jmp SHORT @@end
+@@moveRight:
+	call movePaddleRight
+	jmp SHORT @@end
+@@moveLeft:
+	call movePaddleLeft
+	
+@@end:
+	ret
+ENDP gamelogistic 
+
+;; Generische tekenprocedure
 PROC drawObject
 	ARG @@XPOS:byte, @@YPOS:byte, @@SPRITE:dword, @@WIDTH:byte, @@HEIGHT:byte	; x en y coördinaat in cellen, breedte en hoogte in pixels
-	USES eax, ebx, ecx, esi, edi
+	USES eax, ebx, ecx, edx, esi, edi
 	mov edi, VIDMEMADR
 	mov esi, [@@SPRITE]
 	; begin: positie van eerste pixel op scherm bepalen (omzetting van cellen naar pixels, x en y hebben als "eenheid" cellen)
@@ -227,39 +414,106 @@ PROC drawObject
 	sub eax, ebx 					; eax in row_loop gebruikt om naar de volgende rij te gaan in het videogeheugen
 	movzx ebx, [@@HEIGHT] 			; ebx bepaalt in de volgende loop hoeveel keer we nog moeten itereren
 	
-	@@row_loop:						; voor alle rijen in sprite	
-		movzx ecx, [@@WIDTH]		; aantal bytes/kleurindexen voor 'rep movsb'
-		rep movsb					; bytes/kleurindexen van huidige rij in sprite kopiëren naar videogeheugen
-		add edi, eax				; naar volgende rij gaan in videogeheugen
-		dec ebx
-		jnz @@row_loop
+@@row_loop:						; voor alle rijen in sprite	
+	movzx ecx, [@@WIDTH]		; aantal bytes/kleurindexen voor 'rep movsb'
+	rep movsb					; bytes/kleurindexen van huidige rij in sprite kopiëren naar videogeheugen
+	add edi, eax				; naar volgende rij gaan in videogeheugen
+	dec ebx
+	jnz @@row_loop
 		
 	ret
 ENDP drawObject
 
 PROC drawBall
-	ARG @@STRUCT:dword
 	USES eax, ebx
-	mov ebx, [@@STRUCT]
+	mov ebx, offset ball_object
 	movzx eax, [ebx + Ball.x]
 	movzx ebx, [ebx + Ball.y]
-	call drawObject, eax, ebx, offset ball_array, BALLWIDTH, BALLHEIGHT
+	call drawObject, eax, ebx, offset ball_array, BALLWIDTHPX, BALLHEIGHTPX
 	ret
 ENDP drawBall
 
 PROC drawPaddle
-	ARG @@STRUCT:dword
 	USES eax, ebx
-	mov ebx, [@@STRUCT]
+	mov ebx, offset paddle_object
 	movzx eax, [ebx + Paddle.x]
 	movzx ebx, [ebx + Paddle.y]
-	call drawObject, eax, ebx, offset paddle_array, PADDLEWIDTH, PADDLEHEIGHT
+	call drawObject, eax, ebx, offset paddle_array, PADDLEWIDTHPX, PADDLEHEIGHTPX
 	ret
 ENDP drawPaddle
+
+; PROC determineColor
+	; ARG [@@COUNTER] RETURNS eax
+	; USES eax, edx
+
+; PROC drawStones
+	; USES eax, ebx, ecx, edx
+	; mov ebx, offset stones_array
+	; mov ecx, COLSTONES*ROWSTONES
+; @@drawLoop:
+	; ; posx = STONESSTARTX + (index_position%COLSTONES) * STONEWIDTHCELL
+	; push ecx				; counter OP STACK
+	; xor edx, edx
+	; movzx eax, [ebx + Stone.index]
+	; mov ecx, COLSTONES
+	; div ecx
+	; mov eax, STONEWIDTHCELL
+	; mul edx
+	; add eax, STONESSTARTX
+	; push eax				; x-coördinaat OP STACK
+	; ; posy = STONESSTARTY + (index_position/COLSTONES) * STONEHEIGHTCELL
+	; xor edx, edx
+	; movzx eax, [ebx + Stone.index]
+	; div ecx
+	; mov edx, STONEHEIGHTCELL
+	; mul edx
+	; add eax, STONESSTARTY   ; eax bevat y-coördinaat
+	; pop edx					; x-coördinaat VAN STACK
+	; pop ecx					; counter VAN STACK
+	; push ebx 				; huidige struct OP STACK
+	; call determineColor, ecx 	; returnt pointer naar nodige sprite in ebx
+	; call drawObject, edx, eax, ebx, STONEWIDTHPX, STONEHEIGHTPX
+	; pop ebx					; huidige struct VAN STACK
+	; add ebx, 2				; naar volgende struct gaan
+	; loop @@drawLoop
+	; ret
+; ENDP drawStones
+
+;; Indexen juist zetten
+PROC initStones
+	USES eax, ebx, ecx
+	mov ecx, COLSTONES*ROWSTONES
+	mov ebx, offset stones_array
+	xor eax, eax
+@@arrayLoop:	
+	mov [ebx + Stone.index], al
+	add ebx, 2					; naar volgende struct gaan
+	inc eax
+	loop @@arrayLoop
+	ret
+ENDP initStones
+
+;; Levens displayen (zie compendium)
+PROC displayString
+USES eax, ebx, edx
+	mov edx, 0
+	mov ebx, 0
+	mov ah, 02h
+	shl edx, 08h
+	mov dl, bl
+	mov bh, 0
+	int 10h
+	mov ah, 09h
+	mov edx, offset levens_string
+	int 21h
+	ret
+ENDP displayString
 	
 PROC drawlogistic
-	call drawBall, offset ball_object
-	call drawPaddle, offset paddle_object
+	call displayString
+	;call drawStones 
+	call drawBall
+	call drawPaddle
 	ret
 ENDP drawlogistic
 
@@ -273,29 +527,45 @@ PROC main
 	call setVideoMode, 13h
 	call fillBackground, 0
 	
-	; call __keyb_installKeyboardHandler
+	call __keyb_installKeyboardHandler
 	
 	call openFile, offset ball_file
-	call readChunk, BALLSIZE, offset ball_array
+	call readChunk, BALLSIZEPX, offset ball_array
 	call closeFile
 	
 	call openFile, offset paddle_file
-	call readChunk, PADDLESIZE, offset paddle_array
+	call readChunk, PADDLESIZEPX, offset paddle_array
 	call closeFile
 	
-	call drawlogistic
-	 
-	; ; Alle spelcomponenten tekenen (pedel, bal, grid van stenen).
-	; ; Vervolgens in de spellus gaan.
-	 
-	; @@gameloop:
-		
-		; ; ; call gamelogistic
-		; call drawlogistic
-		
-	; loop @@gameloop
+	call openFile, offset bstone_file
+	call readChunk, STONESIZEPX, offset bstone_array
+	call closeFile
 	
-	call	waitForSpecificKeystroke, 001Bh ; wacht tot de escape-toets wordt ingedrukt
+	call openFile, offset gstone_file
+	call readChunk, STONESIZEPX, offset gstone_array
+	call closeFile
+	
+	call openFile, offset rstone_file
+	call readChunk, STONESIZEPX, offset rstone_array
+	call closeFile
+	
+	call initStones
+	
+	; Handmatig loop maken, we kennen bijvoorbeeld aan eax waarde 1 toe juist voor onze loop.
+	; We blijven iteren zolang eax niet gelijk is aan 0 (jump if not zero).
+	; De procedure gamelogistic geeft bijvoorbeeld steeds een waarde terug die we aan eax kennen, het geeft 0 terug als het spel gedaan is, de speler heeft verloren of gewonnen.
+	 
+	;; ------ GAME LOOP ------
+@@gameloop:
+	
+	call wait_VBLANK
+	call fillBackground, 0
+	call gamelogistic
+	call drawlogistic
+		
+	loop @@gameloop
+	
+	call waitForSpecificKeystroke, 001Bh ; wacht tot de escape-toets wordt ingedrukt
 	call terminateProcess
 ENDP main
 	  
@@ -310,28 +580,30 @@ ENDP main
 ;y position < , > ; een position struct met de standaardwaarden (d.w.z. 0 en 0)
 
 DATASEG
-	ball_object 	Ball <BALLSTARTX, BALLSTARTY> ; min_pos: (0,0), max_pos: (78, 48)
-	paddle_object 	Paddle <PADDLESTARTX, PADDLESTARTY>
+	ball_object 	Ball < >
+	paddle_object 	Paddle < >
+	stones_array    Stone COLSTONES*ROWSTONES dup (< >)
 	
 	ball_file 		db "ball", 0
 	paddle_file		db "paddle", 0
 	bstone_file		db "bstone", 0
 	gstone_file 	db "gstone", 0
 	rstone_file		db "rstone", 0
-	ystone_file		db "ystone", 0
+	;ystone_file		db "ystone", 0
 	
 	openErrorMsg 	db "could not open file", 13, 10, '$'
 	readErrorMsg 	db "could not read data", 13, 10, '$'
 	closeErrorMsg 	db "error during file closing", 13, 10, '$'
+	levens_string	db "Levens:", 7, 10, '$'
 	
 UDATASEG ; unitialised datasegment, zoals declaratie in C
 	filehandle dw ? ; Één filehandle is volgens mij genoeg, aangezien je deze maar één keer nodig zal hebben per bestand kan je die hergebruiken, VRAAG: WAAROM dw ALS DATATYPE?
-	ball_array db BALLSIZE dup (?)
-	paddle_array db PADDLESIZE dup (?)
-	bstone_array db STONESIZE dup (?)
-	gstone_array db STONESIZE dup (?)
-	rstone_array db STONESIZE dup (?)
-	ystone_array db STONESIZE dup (?)
+	ball_array db BALLSIZEPX dup (?)
+	paddle_array db PADDLESIZEPX dup (?)
+	bstone_array db STONESIZEPX dup (?)
+	gstone_array db STONESIZEPX dup (?)
+	rstone_array db STONESIZEPX dup (?)
+	;ystone_array db STONESIZEPX dup (?)
 ; -------------------------------------------------------------------
 ; STACK
 ; -------------------------------------------------------------------
